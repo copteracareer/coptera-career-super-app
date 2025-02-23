a0b094536b041d54766a90f18ae1effb841888fe
Dimas Setiaji <dimassetiaji30@gmail.com>
2025-02-23 15:53:51 +0700

    admin base

--- src/app/(job-portal)/admin/layout.tsx
+++ src/app/(job-portal)/admin/layout.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import KBar from "@/components/kbar";
+import AppSidebar from "@/components/layout/app-sidebar";
+import Header from "@/components/layout/header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
 
-const JobPortalAdminLayout = () => {
-  return <div>JobPortalAdminLayout</div>;
+const JobPortalAdminLayout = ({ children }: { children: React.ReactNode }) => {
+  const cookieStore = cookies();
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true";
+  return (
+    <KBar>
+      <SidebarProvider defaultOpen={defaultOpen}>
+        <AppSidebar />
+        <SidebarInset>
+          <Header />
+          {children}
+        </SidebarInset>
+      </SidebarProvider>
+    </KBar>
+  );
 };
 
 export default JobPortalAdminLayout;
