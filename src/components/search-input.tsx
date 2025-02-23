74796b2a914753380183991b5c12c13211749f51
Dimas Setiaji <dimassetiaji30@gmail.com>
2025-02-23 16:07:25 +0700

    fix bug

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,8 +1,7 @@
-'use client';
-import { Input } from '@/components/ui/input';
-import { useKBar } from 'kbar';
-import { ArrowRight, Search } from 'lucide-react';
-import { Button } from './ui/button';
+"use client";
+import { useKBar } from "kbar";
+import { Search } from "lucide-react";
+import { Button } from "./ui/button";
 
 export default function SearchInput() {
   const { query } = useKBar();
